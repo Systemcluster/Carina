2cbec74ad21344d8ef0b370ea03b6fd91762de55
Christian Sdunek <me@systemcluster.me>
2018-05-11 05:57:52 +0200

    Start implementation

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+//
+// Carina Alpha
+//
+
+#![feature(nll)] // non-lexical lifetime
+#![feature(in_band_lifetimes, nll, nested_method_call)] // simplify lifetimes
+#![feature(try_trait, catch_expr)] // error handling
+#![feature(crate_in_paths, crate_visibility_modifier, non_modrs_mods)] // module handling
+#![feature(decl_macro, proc_macro)] // macro improvements
+#![feature(arbitrary_self_types)] // additional self method arguments
+#![feature(generators, generator_trait)] // generators/coroutines
+#![feature(fn_traits, unboxed_closures)] // function-like type traits
+#![feature(never_type)] // new types
+#![feature(const_fn)] // const functions
+#![feature(const_generics)] // const generics
+
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(non_upper_case_globals)]
+
+
+
+fn main() -> Result<(), std::io::Error> {
+    println!("Hello, world!");
+
+    Ok(())
+}
