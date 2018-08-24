d7adb8ebb4e85492ef784454c9f4b26feea7c77d
Christian Sdunek <me@systemcluster.me>
2018-08-25 00:49:08 +0200

    Update code setup

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@
 // Carina Alpha
 //
 
+#![feature(rust_2018_preview, used)] // rust 2018
 #![feature(nll)] // non-lexical lifetime
 #![feature(in_band_lifetimes)] // simplify lifetimes
 #![feature(try_trait, catch_expr)] // error handling
-#![feature(crate_in_paths, crate_visibility_modifier, non_modrs_mods)] // module handling
 #![feature(decl_macro)] // macro improvements
 #![feature(arbitrary_self_types)] // additional self method arguments
 #![feature(generators, generator_trait)] // generators/coroutines
@@ -18,11 +18,13 @@
 #![allow(unused_variables)]
 #![allow(non_upper_case_globals)]
 
+#[global_allocator] static Allocator: std::alloc::System = std::alloc::System;
+
 
 #[macro_use] extern crate pest;
 #[macro_use] extern crate pest_derive;
 #[macro_use] extern crate failure;
-
+#[macro_use] extern crate simplelog;
 
 use failure::Error;
 
