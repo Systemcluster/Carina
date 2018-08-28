d7576a6125f1641ec0e69e77d14ae203e9fb3ad0
Christian Sdunek <me@systemcluster.me>
2018-08-28 02:34:44 +0200

    Change dependencies

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@
 #![feature(rust_2018_preview, used)] // rust 2018
 #![feature(nll)] // non-lexical lifetime
 #![feature(in_band_lifetimes)] // simplify lifetimes
-#![feature(try_trait, catch_expr)] // error handling
+#![feature(try_trait, try_blocks)] // error handling
 #![feature(decl_macro)] // macro improvements
 #![feature(arbitrary_self_types)] // additional self method arguments
 #![feature(generators, generator_trait)] // generators/coroutines
 #![feature(fn_traits, unboxed_closures)] // function-like type traits
 #![feature(never_type)] // new types
 #![feature(const_fn)] // const functions
+#![feature(label_break_value)]
 
 #![allow(dead_code)]
 #![allow(unused_imports)]
@@ -25,11 +26,15 @@
 #[macro_use] extern crate pest_derive;
 #[macro_use] extern crate failure;
 #[macro_use] extern crate simplelog;
+extern crate ring;
 
 use failure::Error;
 
+
 fn main() -> Result<(), Error> {
     println!("Hello, world!");
+
+
 
     Ok(())
 }
